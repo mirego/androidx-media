2a2c95608786285bd4816c94c429fc52908c740f
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 16:15:55 -0500

    Tunneling reporting
    We want to report wether or not the tunneling is effectively enabled.
    
    This PR adds what the app needs to query this status.
    
    (cherry picked from commit 759288aabf662634a758683d605e26cc33fce32c)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/trackselection/DefaultTrackSelector.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/trackselection/DefaultTrackSelector.java
@@ -2472,6 +2472,12 @@ public class DefaultTrackSelector extends MappingTrackSelector
   private AudioAttributes audioAttributes;
   private @MonotonicNonNull Boolean deviceIsTV;
 
+  // MIREGO added
+  private boolean isTunnelingEffectivelyEnabled = false;
+  public boolean isTunnelingEffectivelyEnabled() {
+    return isTunnelingEffectivelyEnabled;
+  }
+
   /**
    * @param context Any {@link Context}.
    */
@@ -2717,9 +2723,12 @@ public class DefaultTrackSelector extends MappingTrackSelector
     }
 
     // Configure audio and video renderers to use tunneling if appropriate.
+    // MIREGO: set isTunnelingEffectivelyEnabled
     if (parameters.tunnelingEnabled) {
-      maybeConfigureRenderersForTunneling(
+      isTunnelingEffectivelyEnabled = maybeConfigureRenderersForTunneling(
           mappedTrackInfo, rendererFormatSupports, rendererConfigurations, rendererTrackSelections);
+    } else {
+      isTunnelingEffectivelyEnabled = false;
     }
 
     // Configure audio renderer to use offload if appropriate.
@@ -3265,7 +3274,8 @@ public class DefaultTrackSelector extends MappingTrackSelector
    *     ones that enable tunneling as a result of this call.
    * @param trackSelections The renderer track selections.
    */
-  private static void maybeConfigureRenderersForTunneling(
+  // MIREGO: returns result
+  private static boolean maybeConfigureRenderersForTunneling(
       MappedTrackInfo mappedTrackInfo,
       @Capabilities int[][][] rendererFormatSupports,
       @NullableType RendererConfiguration[] rendererConfigurations,
@@ -3307,6 +3317,8 @@ public class DefaultTrackSelector extends MappingTrackSelector
       rendererConfigurations[tunnelingAudioRendererIndex] = tunnelingRendererConfiguration;
       rendererConfigurations[tunnelingVideoRendererIndex] = tunnelingRendererConfiguration;
     }
+
+    return enableTunneling;
   }
 
   /**
