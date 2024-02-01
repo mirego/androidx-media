bf91a60b7b34fee7c991f1aca2e38c8be4870508
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 16:40:13 -0500

    Move onLoadStarted log to avoid triplication
    We were logging once and then once more for every listener.
    Move the log to the caller to avoid that.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/chunk/ChunkSampleStream.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/chunk/ChunkSampleStream.java
@@ -662,8 +662,14 @@ public class ChunkSampleStream<T extends ChunkSource>
     } else if (loadable instanceof InitializationChunk) {
       ((InitializationChunk) loadable).init(chunkOutput);
     }
+
     loader.startLoading(
         loadable, this, loadErrorHandlingPolicy.getMinimumLoadableRetryCount(loadable.type));
+
+    // MIREGO
+    Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"loadStarted (type: %d): startTime: %d endTime: %d duration: %d  (%s)",
+        loadable.type, loadable.startTimeUs / 1000, loadable.endTimeUs / 1000, (loadable.endTimeUs - loadable.startTimeUs) / 1000, loadable.dataSpec.uri);
+
     return true;
   }
 
