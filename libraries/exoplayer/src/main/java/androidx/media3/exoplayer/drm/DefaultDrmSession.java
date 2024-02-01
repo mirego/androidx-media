cc4de72bd13db7e65b405f76a0822be665dc41b4
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 09:07:41 -0500

    Tools and mods to handle DRM reprovisioning
    This PR brings mods and tools to help detecting DRM requests & responses errors and trigger reprovisioning when needed.
    
     Add unprovisionDevice() method on FrameworkMediaDrm to trigger device unprovisioning.
     Add public getter for keyRequestProperties value on HttpMediaDrmCallback.
     Add onProvisionError(...) & onKeyError(...) methods on MediaDrmCallback interface.
     Add calls to MediaDrmCallback$onProvisionError(...) & MediaDrmCallback$onKeyError(...) methods in DrmDefaultSession.
     Create new signature for HttpMediaDrmCallback$executeKeyRequest so it's possible to override license keys (will be useful for us to intercept license headers for integration tests).
    
    (cherry picked from commit 4489a97146be08693fe2b73af174a4e713833da2)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/DefaultDrmSession.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/DefaultDrmSession.java
@@ -571,6 +571,9 @@ import org.checkerframework.checker.nullness.qual.RequiresNonNull;
         || DrmUtil.isFailureToConstructNotProvisionedException(e)) {
       provisioningManager.provisionRequired(this);
     } else {
+      // MIREGO
+      callback.onKeyError(e);
+
       onError(
           e,
           thrownByExoMediaDrm
