1d1f41b0059f6e77d46648c443b97c20bdcc58c3
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 13:42:51 -0500

    Add more logs around most reported non-fatal event
    We have some non-fatal issues reported from the trial.
    Adding more logs around those errors to be able to know the source.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayerImpl.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayerImpl.java
@@ -1021,6 +1021,8 @@ import java.util.concurrent.TimeoutException;
       this.foregroundMode = foregroundMode;
       if (!internalPlayer.setForegroundMode(foregroundMode)) {
         // One of the renderers timed out releasing its resources.
+        // MIREGO
+        Log.e(TAG, String.format("setForegroundMode(%s), ERROR_CODE_TIMEOUT", foregroundMode));
         stopInternal(
             ExoPlaybackException.createForUnexpected(
                 new ExoTimeoutException(ExoTimeoutException.TIMEOUT_OPERATION_SET_FOREGROUND_MODE),
@@ -1060,6 +1062,8 @@ import java.util.concurrent.TimeoutException;
     audioFocusManager.release();
     if (!internalPlayer.release()) {
       // One of the renderers timed out releasing its resources.
+      // MIREGO
+      Log.e(TAG, "release(), ERROR_CODE_TIMEOUT");
       listeners.sendEvent(
           Player.EVENT_PLAYER_ERROR,
           listener ->
@@ -2735,6 +2739,10 @@ import java.util.concurrent.TimeoutException;
     }
     this.videoOutput = videoOutput;
     if (messageDeliveryTimedOut) {
+      // MIREGO
+      Log.e(TAG,
+          String.format("setVideoOutputInternal() detachSurfaceTimeoutMs: %s, ERROR_CODE_TIMEOUT",
+              detachSurfaceTimeoutMs));
       stopInternal(
           ExoPlaybackException.createForUnexpected(
               new ExoTimeoutException(ExoTimeoutException.TIMEOUT_OPERATION_DETACH_SURFACE),
