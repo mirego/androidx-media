7f793d9c3ef7c2e74aea168fc1db060afab3fc37
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 13:55:14 -0500

    FONM-17023: ABR is changing layer too often
    The bandwidth meter computes a weight for every measurements and uses the weight to determine how many samples to keep.
    Because the UHD streaming chunks are so big, we were only keeping 2 measurements, and using the slower one as the bandwidth measurement most of the time. That made the system way too sensitive to isolated slowdowns.
    
    Now we keep a min number of samples to compute bandwidth. It's currently 7, so if they were all of equal size, we'd always use the one in the middle as the bandwidth measurement.
    Also added some logs related to ABR.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/upstream/DefaultBandwidthMeter.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/upstream/DefaultBandwidthMeter.java
@@ -26,6 +26,7 @@ import androidx.media3.common.C;
 import androidx.media3.common.util.Assertions;
 import androidx.media3.common.util.BackgroundExecutor;
 import androidx.media3.common.util.Clock;
+import androidx.media3.common.util.Log;
 import androidx.media3.common.util.NetworkTypeObserver;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
@@ -51,6 +52,8 @@ import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
  */
 @UnstableApi
 public final class DefaultBandwidthMeter implements BandwidthMeter, TransferListener {
+
+  private static final String TAG = "BandwidthMeter";
 
   /** Default initial Wifi bitrate estimate in bits per second. */
   public static final ImmutableList<Long> DEFAULT_INITIAL_BITRATE_ESTIMATES_WIFI =
@@ -405,6 +408,11 @@ public final class DefaultBandwidthMeter implements BandwidthMeter, TransferList
     int sampleElapsedTimeMs = (int) (nowMs - sampleStartTimeMs);
     totalElapsedTimeMs += sampleElapsedTimeMs;
     totalBytesTransferred += sampleBytesTransferred;
+
+    // MIREGO
+    Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "onTransferEnd sampleElapsedTimeMs: %d  sampleBytesTransferred: %d",
+        sampleElapsedTimeMs, sampleBytesTransferred);
+
     if (sampleElapsedTimeMs > 0) {
       float bitsPerSecond = (sampleBytesTransferred * 8000f) / sampleElapsedTimeMs;
       slidingPercentile.addSample((int) Math.sqrt(sampleBytesTransferred), bitsPerSecond);
