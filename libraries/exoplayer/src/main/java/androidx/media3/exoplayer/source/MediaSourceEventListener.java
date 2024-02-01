c126c4229ea1c4f0cff4476f0c79e24bc8c21faa
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 16:40:13 -0500

    Move onLoadStarted log to avoid triplication
    We were logging once and then once more for every listener.
    Move the log to the caller to avoid that.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/MediaSourceEventListener.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/source/MediaSourceEventListener.java
@@ -264,9 +264,6 @@ public interface MediaSourceEventListener {
 
     /** Dispatches {@link #onLoadStarted(int, MediaPeriodId, LoadEventInfo, MediaLoadData)}. */
     public void loadStarted(LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData) {
-      // MIREGO
-      Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"loadStarted (type: %d): startTime: %d endTime: %d duration: %d  (%s)", mediaLoadData.dataType, mediaLoadData.mediaStartTimeMs, mediaLoadData.mediaEndTimeMs, mediaLoadData.mediaEndTimeMs - mediaLoadData.mediaStartTimeMs, loadEventInfo.uri);
-
       dispatchEvent(
           (listener) ->
               listener.onLoadStarted(windowIndex, mediaPeriodId, loadEventInfo, mediaLoadData));
