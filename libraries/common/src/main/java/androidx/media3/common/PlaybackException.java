593cee7fa5b23e1991aed9a68f6d49169c2d5b4d
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 14:38:41 -0500

    Workaround audio sample rate stream issue
    We are having sporadic issues with streams having inconsistent audio sample rates. The container and the data are at 48 kHz, but the atom dac3 has the sample rate at 32 kHz. That messes up with the sync mechanisms and breaks the playback.
    
    This PR adds a workaround the issue, forcing the dac3 output to 48 kHz when it's inconsistent with the container (can be enabled by setting a global flag in Util, disabled by default)
    
    Also added a new function in Log, to be able to log an exception, which allows us to notify the app when the sample rate error happens.
    
    Also kept some of the logs improvements I made during the investigation.

--- libraries/common/src/main/java/androidx/media3/common/PlaybackException.java
+++ libraries/common/src/main/java/androidx/media3/common/PlaybackException.java
@@ -93,6 +93,7 @@ public class PlaybackException extends Exception {
         ERROR_CODE_DECODING_FORMAT_EXCEEDS_CAPABILITIES,
         ERROR_CODE_DECODING_FORMAT_UNSUPPORTED,
         ERROR_CODE_DECODING_RESOURCES_RECLAIMED,
+        ERROR_CODE_AUDIO_TRACK_INCONSISTENT_SAMPLE_RATE,
         ERROR_CODE_AUDIO_TRACK_INIT_FAILED,
         ERROR_CODE_AUDIO_TRACK_WRITE_FAILED,
         ERROR_CODE_AUDIO_TRACK_OFFLOAD_INIT_FAILED,
@@ -290,6 +291,9 @@ public class PlaybackException extends Exception {
 
   /** Caused by an AudioTrack init operation failure in offload mode. */
   public static final int ERROR_CODE_AUDIO_TRACK_OFFLOAD_INIT_FAILED = 5004;
+
+  /** MIREGO: Caused by an inconsistent sample rate between the container and the track atom (recoverable). */
+  public static final int ERROR_CODE_AUDIO_TRACK_INCONSISTENT_SAMPLE_RATE = 5901;
 
   // DRM errors (6xxx).
 
