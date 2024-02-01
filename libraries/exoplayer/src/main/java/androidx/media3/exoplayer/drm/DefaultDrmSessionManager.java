4489a97146be08693fe2b73af174a4e713833da2
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-01 09:07:41 -0500

    Tools and mods to handle DRM reprovisioning
    This PR brings mods and tools to help detecting DRM requests & responses errors and trigger reprovisioning when needed.
    
     Add unprovisionDevice() method on FrameworkMediaDrm to trigger device unprovisioning.
     Add public getter for keyRequestProperties value on HttpMediaDrmCallback.
     Add onProvisionError(...) & onKeyError(...) methods on MediaDrmCallback interface.
     Add calls to MediaDrmCallback$onProvisionError(...) & MediaDrmCallback$onKeyError(...) methods in DrmDefaultSession.
     Create new signature for HttpMediaDrmCallback$executeKeyRequest so it's possible to override license keys (will be useful for us to intercept license headers for integration tests).

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/DefaultDrmSessionManager.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/DefaultDrmSessionManager.java
@@ -848,6 +848,9 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
 
     @Override
     public void onProvisionError(Exception error, boolean thrownByExoMediaDrm) {
+      // MIREGO
+      callback.onProvisionError(error);
+
       provisioningSession = null;
       ImmutableList<DefaultDrmSession> sessionsToNotify =
           ImmutableList.copyOf(sessionsAwaitingProvisioning);
--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/FrameworkMediaDrm.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/drm/FrameworkMediaDrm.java
@@ -621,4 +621,14 @@ public final class FrameworkMediaDrm implements ExoMediaDrm {
       }
     }
   }
+
+  // MIREGO
+  public void unprovisionDevice() throws DeniedByServerException {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return;
+    }
+
+    byte[] response = "unprovision".getBytes();
+    provideProvisionResponse(response);
+  }
 }
