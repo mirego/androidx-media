33cb967b775f57b65e24b759c52d4c73bbb4a2d6
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-05 13:56:58 -0500

    Add okhttp to the release build
    We're adding the option of using okhttp for exoplayer in our app. It wasn't built in our release action.
    
    This PR adds the okhttp extension to our release builds.
    Also adding a few logs in both http data sources we might use.
    
    (cherry picked from commit 66a910a868ff9f95facfbc7f6fd7978f4143fd84)
    (cherry picked from commit bf4bcf105f15a2da86bd52efb3a125863a2dfe3d)

--- libraries/datasource_okhttp/src/main/java/androidx/media3/datasource/okhttp/OkHttpDataSource.java
+++ libraries/datasource_okhttp/src/main/java/androidx/media3/datasource/okhttp/OkHttpDataSource.java
@@ -275,6 +275,10 @@ public class OkHttpDataSource extends BaseDataSource implements HttpDataSource {
     Response response;
     ResponseBody responseBody;
     Call call = callFactory.newCall(request);
+
+    // MIREGO
+    Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "open dataSource: %s", dataSpec.uri.getPath());
+
     try {
       this.response = executeCall(call);
       response = this.response;
@@ -311,6 +315,10 @@ public class OkHttpDataSource extends BaseDataSource implements HttpDataSource {
       }
       Map<String, List<String>> headers = response.headers().toMultimap();
       closeConnectionQuietly();
+
+      // MIREGO added
+      Log.e(TAG, String.format("HTTP response error %d for %s", responseCode, dataSpec.uri.getPath()));
+
       @Nullable
       IOException cause =
           responseCode == 416
