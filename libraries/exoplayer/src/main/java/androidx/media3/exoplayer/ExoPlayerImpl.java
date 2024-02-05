3615316786ddedb7ca08d20493870b98d93d9b6d
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-05 13:49:55 -0500

    Add queued video frames count (analytics)
    We're using dropped frames as a stream quality measure. Being able to use the ratio of dropped frames over total frames would be better.
    
    This PR adds total queued video frames to the metrics the app has access to.
    
    (cherry picked from commit c64f5f802e98d8dedcb3e1b929862f9357ff95d2)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayerImpl.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayerImpl.java
@@ -3359,6 +3359,12 @@ import java.util.function.IntConsumer;
       analyticsCollector.onDroppedFrames(count, elapsed);
     }
 
+    //MIREGO added
+    @Override
+    public void onQueuedFrames(int count, long elapsed) {
+      analyticsCollector.onQueuedFrames(count, elapsed);
+    }
+
     @Override
     public void onVideoSizeChanged(VideoSize newVideoSize) {
       videoSize = newVideoSize;
