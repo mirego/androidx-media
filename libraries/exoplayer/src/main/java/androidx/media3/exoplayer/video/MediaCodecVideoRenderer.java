35fe64657ac7c9576cb39228e5db4c6c51e84a63
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-05 14:25:01 -0500

    Some more AV sync logs
    Still have reports of AV desync issues. Doesn't look like it's happening in Exo, but adding some more logs to get more data on the video rendering.
    This adds a more detailed log once every second (verbose 1) with some relevant AV sync info.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
@@ -1476,6 +1476,8 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer
     }
   }
 
+  private long lastLogProcessOutputBufferMs = 0; // MIREGO
+
   @Override
   protected boolean processOutputBuffer(
       long positionUs,
