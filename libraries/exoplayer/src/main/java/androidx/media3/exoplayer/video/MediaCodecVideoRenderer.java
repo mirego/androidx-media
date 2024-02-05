309ba2f0711cb32d636cfb281d15b7816da66aa3
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-05 13:31:01 -0500

    Some more ABR logs
    Adding more verbose logs to investigate an abr issue
    
    (cherry picked from commit 351ca5c6ce3fd5e20ac0cc8f356912c74472ab2c)
    (cherry picked from commit 3b40c4a16717e8b1d93448555afabf968e5f326a)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
@@ -854,10 +854,19 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer
   protected List<MediaCodecInfo> getDecoderInfos(
       MediaCodecSelector mediaCodecSelector, Format format, boolean requiresSecureDecoder)
       throws DecoderQueryException {
-    return MediaCodecUtil.getDecoderInfosSortedByFormatSupport(
-        context,
-        getDecoderInfos(context, mediaCodecSelector, format, requiresSecureDecoder, tunneling),
-        format);
+    List<MediaCodecInfo> mediaCodecInfoList =
+        MediaCodecUtil.getDecoderInfosSortedByFormatSupport(
+            context,
+            getDecoderInfos(context, mediaCodecSelector, format, requiresSecureDecoder, tunneling),
+            format);
+
+    // MIREGO START
+    for (MediaCodecInfo info: mediaCodecInfoList) {
+      Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "listing codec infos for format %s: %s", format, info.name);
+    }
+    // MIREGO END
+
+    return mediaCodecInfoList;
   }
 
   // Other methods
