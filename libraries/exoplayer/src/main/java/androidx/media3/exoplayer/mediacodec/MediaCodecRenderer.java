ea5f24a984cbaf6848f0a398fa4d88ab38434fc4
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-05 15:19:37 -0500

    Fix issue when switching from a decrypt-only audio pipeline to a bypass
    We encountered an issue with DLAR on some devices when the audio format was different from the main feed. This was caused by the audio sink using the wrong format after a switch from a decrypt-only codec to a bypass, when the format was different between the 2.
    
    When the audio pipeline is using a decrypt-only codec, a field is set in the audio renderer to keep the decrypt-only format. That field is used, among other things, to set the audio sink format.
    
    When using a bypass, the codec is not used, we send the data directly to the sink.
    
    The issue was caused by the decrypt-only format field not being reset when we enable bypass. Because of that, the audio sink was set using the obsolete format, causing it to expect a format different than the one we were feeding it.
    
    This PR fixes it by properly resetting the field when enabling bypass. Also kept some logs added to investigate.

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/mediacodec/MediaCodecRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/mediacodec/MediaCodecRenderer.java
@@ -1251,7 +1251,8 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
   }
 
   /** Configures rendering where no codec is used. */
-  private void initBypass(Format format) {
+  // MIREGO: made protected
+  protected void initBypass(Format format) {
     // MIREGO
     Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"initBypass format: %s (%s)", format, this);
 
@@ -1283,6 +1284,10 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
     }
     onReadyToInitializeCodec(inputFormat);
     codecInitializingTimestamp = getClock().elapsedRealtime();
+
+    // MIREGO
+    Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "initCodec format: %s", inputFormat);
+
     MediaCodecAdapter.Configuration configuration =
         getMediaCodecConfiguration(codecInfo, inputFormat, crypto, codecOperatingRate);
     if (Util.SDK_INT >= 31) {
@@ -1706,6 +1711,9 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
     inputFormat = newFormat;
 
     if (bypassEnabled) {
+      // MIREGO
+      Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "Bypass enabled - Drain and reinitialize");
+
       bypassDrainAndReinitialize = true;
       return null; // Need to drain batch buffer first.
     }
@@ -1727,6 +1735,9 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
 
     Format oldFormat = checkNotNull(codecInputFormat);
     if (drmNeedsCodecReinitialization(codecInfo, newFormat, codecDrmSession, sourceDrmSession)) {
+      // MIREGO
+      Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "onInputFormatChanged drmNeedsCodecReinitialization");
+
       drainAndReinitializeCodec();
       return new DecoderReuseEvaluation(
           codecInfo.name,
@@ -1742,9 +1753,15 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
     @DecoderDiscardReasons int overridingDiscardReasons = 0;
     switch (evaluation.result) {
       case REUSE_RESULT_NO:
+        // MIREGO
+        Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"onInputFormatChanged do not reuse codec");
+
         drainAndReinitializeCodec();
         break;
       case REUSE_RESULT_YES_WITH_FLUSH:
+        // MIREGO
+        Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"onInputFormatChanged flush codec");
+
         if (!updateCodecOperatingRate(newFormat)) {
           overridingDiscardReasons |= DISCARD_REASON_OPERATING_RATE_CHANGED;
         } else {
@@ -1759,6 +1776,9 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
         }
         break;
       case REUSE_RESULT_YES_WITH_RECONFIGURATION:
+        // MIREGO
+        Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"onInputFormatChanged reconfigure codec");
+
         if (!updateCodecOperatingRate(newFormat)) {
           overridingDiscardReasons |= DISCARD_REASON_OPERATING_RATE_CHANGED;
         } else {
@@ -1776,6 +1796,9 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
         }
         break;
       case REUSE_RESULT_YES_WITHOUT_RECONFIGURATION:
+        // MIREGO
+        Log.v(Log.LOG_LEVEL_VERBOSE1, TAG,"onInputFormatChanged reuse codec");
+
         if (!updateCodecOperatingRate(newFormat)) {
           overridingDiscardReasons |= DISCARD_REASON_OPERATING_RATE_CHANGED;
         } else {
@@ -2074,7 +2097,13 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
    * @throws ExoPlaybackException If an error occurs re-initializing a codec.
    */
   private void drainAndReinitializeCodec() throws ExoPlaybackException {
+    // MIREGO
+    Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "drainAndReinitializeCodec");
+
     if (codecReceivedBuffers) {
+      // MIREGO
+      Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "setting codecDrainState and codecDrainAction");
+
       codecDrainState = DRAIN_STATE_SIGNAL_END_OF_STREAM;
       codecDrainAction = DRAIN_ACTION_REINITIALIZE;
     } else {
@@ -2468,6 +2497,9 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
   }
 
   private void reinitializeCodec() throws ExoPlaybackException {
+    // MIREGO
+    Log.v(Log.LOG_LEVEL_VERBOSE1, TAG, "reinitializeCodec");
+
     releaseCodec();
     maybeInitCodecOrBypass();
   }
