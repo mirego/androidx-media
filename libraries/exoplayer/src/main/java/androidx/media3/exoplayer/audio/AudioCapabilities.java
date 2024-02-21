869388b9fb1cce939c5cf7c3cc01f3a583c3bdd2
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2024-02-21 10:19:53 -0500

    FTVPI-2396: Toshiba Fire TV edition switch to BT while playing Dolby doesn't work (#10)
    This PR is a port from the [original Amazon fix on their
    Exoplayer](https://github.com/amzn/exoplayer-amazon-port/commit/e341944d1ee71eecc776425a0fcf3c9bc2669e46)
    
    [Problem]
    Switching audio from dolby out to bt headset causes playback to stop
    when using the demo app. In FOS5 the platform would send an intent
    HDMI_AUDIO_PLUG when audio was switched from dolby out to BT headset.
    This does not happen in FOS6 devices like Toshiba Fire TV edition.
    
    [Solution]
    Add new logic for Toshiba Fire TV edition use cases. Platform will
    expose a new flag 'use_external_surround_sound_flag' on Toshiba Fire TV
    edition and newer Fire Televisions. If this flag is set then Exo player
    should only depend on the setting 'external_surround_sound_enabled' to
    determine whether an audio output supports dolby.
    
    If the 'use_external_surround_sound_flag' flag is not set. Then Exo
    player will internally also register for HDMI_AUDIO_PLUG intent. When
    the 'external_surround_sound_enabled' is set it indicates that device is
    connected to a dolby capable output. But when
    'external_surround_sound_enabled' is not set, then Exoplayer fallsback
    to the audio capabilities from the most recent HDMI_AUDIO_PLUG intent.
    
    Add the onAudioCapabilities callback to the demo app. In this callback
    the demo app rebuilds the the media pipeline when there is a change in
    audio output deivce.
    
    [Devices]
    FOS6 fire tv
    
    cr: https://code.amazon.com/reviews/CR-91994048

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/audio/AudioCapabilities.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/audio/AudioCapabilities.java
@@ -18,6 +18,8 @@ package androidx.media3.exoplayer.audio;
 import static androidx.media3.common.util.Assertions.checkNotNull;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -81,6 +83,22 @@ public final class AudioCapabilities {
   /** Global settings key for devices that can specify external surround sound. */
   private static final String EXTERNAL_SURROUND_SOUND_KEY = "external_surround_sound_enabled";
 
+  // MIREGO - AMZN_CHANGE_BEGIN
+  public static final AudioCapabilities EXTERNAL_SURROUND_SOUND_CAPABILITIES =
+      new AudioCapabilities(
+          new int[]{
+              AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_AC3, AudioFormat.ENCODING_E_AC3
+          },
+          DEFAULT_MAX_CHANNEL_COUNT);
+
+  /**
+   * For Optical output, we read this global setting to detect if dolby
+   * output is enabled. If USE_EXTERNAL_SURROUND_SOUND_FLAG is not set, then
+   * we fallback on the HDMI audio intent.
+   */
+  public static final String USE_EXTERNAL_SURROUND_SOUND_FLAG = "use_external_surround_sound_flag";
+  // MIREGO - AMZN_CHANGE_END
+
   /**
    * Returns the current audio capabilities for the device.
    *
@@ -102,6 +120,25 @@ public final class AudioCapabilities {
     if (Util.SDK_INT >= 23 && Api23.isBluetoothConnected(context)) {
       return DEFAULT_AUDIO_CAPABILITIES;
     }
+
+    // MIREGO - AMZN_CHANGE_BEGIN
+    boolean useSurroundSoundFlag = false;
+    boolean isSurroundSoundEnabled = false;
+
+    // read global surround sound amazon specific settings
+    if (Util.SDK_INT >= 17) {
+      ContentResolver resolver = context.getContentResolver();
+      useSurroundSoundFlag = useSurroundSoundFlagV17(resolver);
+      isSurroundSoundEnabled = isSurroundSoundEnabledV17(resolver);
+    }
+    //  If use surround sound enabled flag is set, then ignore the hmdi plug
+    //  encodings.  Rely only on EXTERNAL_SURROUND_SOUND_CAPABILITIES to
+    //  determine if dolby is supported.
+    if (useSurroundSoundFlag) {
+      return isSurroundSoundEnabled ? EXTERNAL_SURROUND_SOUND_CAPABILITIES :
+          DEFAULT_AUDIO_CAPABILITIES;
+    }
+    // MIREGO - AMZN_CHANGE_END
 
     ImmutableSet.Builder<Integer> supportedEncodings = new ImmutableSet.Builder<>();
     if (deviceMaySetExternalSurroundSoundGlobalSetting()
@@ -136,6 +173,20 @@ public final class AudioCapabilities {
     }
     return DEFAULT_AUDIO_CAPABILITIES;
   }
+
+  // MIREGO - AMZN_CHANGE_BEGIN
+  @TargetApi(17)
+  public static boolean useSurroundSoundFlagV17(ContentResolver
+      resolver) {
+    return Global.getInt(resolver, USE_EXTERNAL_SURROUND_SOUND_FLAG,
+        0) == 1;
+  }
+
+  @TargetApi(17)
+  public static boolean isSurroundSoundEnabledV17(ContentResolver resolver) {
+    return Global.getInt(resolver, EXTERNAL_SURROUND_SOUND_KEY, 0) == 1;
+  }
+  // MIREGO - AMZN_CHANGE_END
 
   /**
    * Returns the global settings {@link Uri} used by the device to specify external surround sound,
