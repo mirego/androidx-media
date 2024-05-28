51938c96e73b56a876e311006af99eb0246e3580
Sylvain Morel <smorel@mirego.com>
2024-05-28 19:42:57 -0400

    Fix issue introduced by previous cherry-pick (removed a needed line)

--- libraries/common/src/main/java/androidx/media3/common/util/Util.java
+++ libraries/common/src/main/java/androidx/media3/common/util/Util.java
@@ -173,6 +173,7 @@ public final class Util {
 
   // MIREGO
   public static boolean shouldWorkaroundAudioSampleRateDataBug = false;
+  public static int timeSinceLastVideoRenderToLogErrorMs = 1000;
   public static int audioVideoDeltaToLogErrorMs = 750;
   public static boolean shouldIgnoreCodecFpsLimitForResolution = false; // MIREGO ADDED
   public static boolean useDifferentAudioSessionIdForTunneling = false; // MIREGO ADDED
