eb6fab9e06742823de2c6473f3750abfe87e3065
SylvainMorel <62572068+SylvainMorel@users.noreply.github.com>
2024-06-17 07:55:17 -0400

    Add debug info to help debugging a pipeline stall issue  (2) (#25)
    In #23, we've added some debug info to help debug an issue where the
    rendering pipeline seems to be stalled.
    
    This PR adds the processed buffer count to the debug info and also
    renamed the queued input buffer count to make it clearer.
    
    (cherry picked from commit 0c8e53467deaaff74cbd8ee4d4f0a37a3e43b66e)
    (cherry picked from commit 350b67d7c8cd39638316abd8226da7e06eaebcf1)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/video/MediaCodecVideoRenderer.java
@@ -1219,7 +1219,8 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer
     hasNotifiedAvDesyncError = false;
     hasNotifiedAvDesyncSkippedFramesError = false;
     queuedFrames = 0;
-    Util.currentAccumulatedVideoQueuedFrames = 0;
+    Util.currentQueuedInputBuffers = 0;
+    Util.currentProcessedOutputBuffers = 0;
 
     videoFrameReleaseControl.onStarted();
   }
@@ -1798,7 +1799,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer
 
     // MIREGO: added
     queuedFrames++;
-    Util.currentAccumulatedVideoQueuedFrames++;
+    Util.currentQueuedInputBuffers++;
     if (queuedFrames >= NOTIFY_QUEUED_FRAMES_THRESHOLD) {
       maybeNotifyQueuedFrames();
     }
@@ -2272,6 +2273,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer
   @CallSuper
   @Override
   protected void onProcessedOutputBuffer(long presentationTimeUs) {
+    Util.currentProcessedOutputBuffers++;
     super.onProcessedOutputBuffer(presentationTimeUs);
     if (!tunneling) {
       buffersInCodecCount--;
