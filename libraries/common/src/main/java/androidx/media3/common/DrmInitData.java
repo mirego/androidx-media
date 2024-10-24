4472749924b25014417ea94943cd6dd7c8cb34f2
SylvainMorel <62572068+SylvainMorel@users.noreply.github.com>
2024-10-24 09:26:45 -0400

    Add support for multiple offline DRM keys (#28)
    We need to support multiple DRM keys for different time periods on
    offline playbacks. This wasn't supported by ExoPlayer.
    
    This PR adds support for multiple keys.
    - Added a new setMode function in DefaultDrmSessionManager to be able to
    pass the needed data (lists of drmKeys and list of hash codes to find
    the right key for a given format)
    - Replaced the single keySetId in DefaultDrmSessionManager by the above
    2 lists
    - When we acquire a session with a new format, use the hash codes list
    to find the index of the right key to use, instead of using the single
    offline key.
    
    Notes:
    - I had to add a field in DefaultDrmSession to be able to reuse the
    session. The existing schemeDatas is used in multiple ways, so changing
    it to keep the scheme datas even for offline playback would have
    required more changes (we try to limit the scope of changes to
    facilitate merges)
    - I initially also made the similar changes in MediaItem (which is given
    the offline key). But after investigation, turns out this code path is
    useless for us, we override or handle ourselves all the related object
    creation. So for the same reason as the previous point, I decided to
    revert those changes.

--- libraries/common/src/main/java/androidx/media3/common/DrmInitData.java
+++ libraries/common/src/main/java/androidx/media3/common/DrmInitData.java
@@ -371,6 +371,18 @@ public final class DrmInitData implements Comparator<SchemeData>, Parcelable {
       return hashCode;
     }
 
+    // MIREGO: added toString
+    @Override
+    public String toString() {
+      return "SchemeData{" +
+          "hashCode=" + hashCode +
+          ", uuid=" + uuid +
+          ", licenseServerUrl='" + licenseServerUrl + '\'' +
+          ", mimeType='" + mimeType + '\'' +
+          ", data=" + Arrays.toString(data) +
+          '}';
+    }
+
     // Parcelable implementation.
 
     @Override
