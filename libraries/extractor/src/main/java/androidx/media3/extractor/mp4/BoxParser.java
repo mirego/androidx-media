c36061f64574b5660efd78bcaaf4ca585e7485e7
Mathieu Lavoie-Lamer <mlavoie@mirego.com>
2026-02-12 10:06:05 -0500

    Fix DV fallback codec playback
    Cherry-pick from 8546cc3

--- libraries/extractor/src/main/java/androidx/media3/extractor/mp4/BoxParser.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/mp4/BoxParser.java
@@ -16,9 +16,9 @@
 package androidx.media3.extractor.mp4;
 
 import static androidx.media3.common.MimeTypes.getMimeTypeFromMp4ObjectType;
-import static androidx.media3.common.util.Assertions.checkNotNull;
-import static androidx.media3.common.util.Assertions.checkState;
 import static androidx.media3.common.util.Util.castNonNull;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import static java.lang.Math.max;
 import static java.nio.ByteOrder.BIG_ENDIAN;
 import static java.nio.ByteOrder.LITTLE_ENDIAN;
@@ -1354,6 +1354,7 @@ public final class BoxParser {
     @C.ColorTransfer int colorTransfer = Format.NO_VALUE;
     // The format of HDR static info is defined in CTA-861-G:2017, Table 45.
     @Nullable ByteBuffer hdrStaticInfo = null;
+    @Nullable DolbyVisionConfig dolbyVisionConfig = null;
 
     while (childPosition - position < size) {
       parent.setPosition(childPosition);
@@ -1478,26 +1479,7 @@ public final class BoxParser {
       } else if (childAtomType == Mp4Box.TYPE_dvcC
           || childAtomType == Mp4Box.TYPE_dvvC
           || childAtomType == Mp4Box.TYPE_dvwC) {
-        int childAtomBodySize = childAtomSize - Mp4Box.HEADER_SIZE;
-        byte[] initializationDataChunk = new byte[childAtomBodySize];
-        parent.readBytes(initializationDataChunk, /* offset= */ 0, childAtomBodySize);
-        // Add the initialization data of Dolby Vision to the existing list of initialization data.
-        if (initializationData != null) {
-          initializationData =
-              ImmutableList.<byte[]>builder()
-                  .addAll(initializationData)
-                  .add(initializationDataChunk)
-                  .build();
-        } else {
-          ExtractorUtil.checkContainerInput(
-              false, "initializationData must already be set from hvcC or avcC atom");
-        }
-        parent.setPosition(childStartPosition + Mp4Box.HEADER_SIZE);
-        @Nullable DolbyVisionConfig dolbyVisionConfig = DolbyVisionConfig.parse(parent);
-        if (dolbyVisionConfig != null) {
-          codecs = dolbyVisionConfig.codecs;
-          mimeType = MimeTypes.VIDEO_DOLBY_VISION;
-        }
+        dolbyVisionConfig = DolbyVisionConfig.parse(parent);
       } else if (childAtomType == Mp4Box.TYPE_vpcC) {
         ExtractorUtil.checkContainerInput(mimeType == null, /* message= */ null);
         mimeType = (atomType == Mp4Box.TYPE_vp08) ? MimeTypes.VIDEO_VP8 : MimeTypes.VIDEO_VP9;
@@ -1663,6 +1645,11 @@ public final class BoxParser {
         }
       }
       childPosition += childAtomSize;
+    }
+
+    if (dolbyVisionConfig != null) {
+      mimeType = MimeTypes.VIDEO_DOLBY_VISION;
+      codecs = dolbyVisionConfig.codecs;
     }
 
     // If the media type was not recognized, ignore the track.
