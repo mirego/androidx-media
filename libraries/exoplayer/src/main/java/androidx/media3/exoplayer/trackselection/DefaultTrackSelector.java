d6c9d0ef6b53f53423be15d93c62ef2c8cbb15ca
Sylvain Morel <smorel@mirego.com>
2026-03-14 17:41:39 -0400

    move stuff

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/trackselection/DefaultTrackSelector.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/trackselection/DefaultTrackSelector.java
@@ -3056,12 +3056,12 @@ public class DefaultTrackSelector extends MappingTrackSelector
     ArrayList<List<T>> possibleSelections = new ArrayList<>();
     int rendererCount = mappedTrackInfo.getRendererCount();
 
-    boolean hasNoEligibleTrack = false; // MIREGO added to notify error when all video tracks are restricted
-
     // MIREGO: skip track selection if the type is disabled. We want to avoid the useless ERROR_CODE_NO_VIDEO_TRACK_ELIGIBLE error (and also the processing)
     if (parameters.disabledTrackTypes.contains(trackType)) {
       return null;
     }
+
+    boolean hasNoEligibleTrack = false; // MIREGO added to notify error when all video tracks are restricted
 
     for (int rendererIndex = 0; rendererIndex < rendererCount; rendererIndex++) {
       if (trackType == mappedTrackInfo.getRendererType(rendererIndex)) {
